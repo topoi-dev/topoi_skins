c4699c0c84dc58fa17434e2c71dd909b858fe56a
Thomas Opolski <metasynthofficial@gmail.com>
2020-12-30 02:16:59 -0500

    fix overflow

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ window.onload = function () {
     $(".intro").fadeOut(1000);
     setTimeout(function() {
       $(".main-pt").removeClass("d-none");
-      $(".main-pt").fadeIn(1000);
+      $(".main-pt").fadeIn(1000,checkOverflow);
     }, 1000);
   });
 
-  $(".dl-btn").click(function () {
+  var windowChange = _.debounce(checkOverflow,50);
 
-  })
+  $(window).on("resize",windowChange);
+
+  //remove shading if body overflow
+  function checkOverflow() {
+    if ($("body").prop('scrollHeight') > $("body").height()) {
+      $("body").css("box-shadow", "none")
+    } else {
+      $("body").css("box-shadow", "inset 0 0 5rem rgba(0, 0, 0, 0.5)")
+    }
+  }
+ 
 };
